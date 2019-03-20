ac136a58e843d124419b313a1395e08e0c84d670
Martin Meredith <martin.meredith@magic42.co.uk>
2019-03-20 09:20:33 +0000

    Add more GNU Paths

--- .zshrc.d/99-gnu-path.zsh
+++ .zshrc.d/99-gnu-path.zsh
@@ -1,5 +1,6 @@
+export PATH="/usr/local/opt/grep/libexec/gnubin:$PATH"
+export PATH="/usr/local/opt/gnu-which/libexec/gnubin:$PATH"
 export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
 export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
 
 alias ls="gls --color=always"
-
