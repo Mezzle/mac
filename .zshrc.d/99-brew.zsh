6d61f219151c76de1bb97b7d4ba2637a1f096db3
Martin Meredith <martin.meredith@substrakthealth.com>
2020-09-14 09:48:38 +0100

    Update to use new brew command

--- .zshrc.d/99-brew.zsh
+++ .zshrc.d/99-brew.zsh
@@ -1,2 +1,2 @@
-    alias brewup='brew update; brew upgrade; brew cask upgrade; brew cleanup; brew doctor'
+    alias brewup='brew update; brew upgrade; brew upgrade --cask; brew cleanup; brew doctor'
     export PATH=/usr/local/sbin:$PATH
